b8fe171d637819cf896244f4454c7d0ad5b0c5c7
SergeyMarcum <sergey.marcum@gmail.com>
2025-04-27 23:23:57 +0500

    Fix code auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
 // src/App.tsx
 import { useEffect } from "react";
-import type { JSX } from "react";
-import { useLocation } from "react-router-dom";
 import { useAuthStore } from "@features/auth/model/store";
 import { AppRoutes } from "@app/routes";
 import { AppInit } from "@app/init";
+import { CircularProgress, Box } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import type { JSX } from "react";
 
 export function App(): JSX.Element {
-  const { isAuthenticated, initAuth } = useAuthStore();
-  const location = useLocation();
+  const { isSessionChecking, initAuth } = useAuthStore();
 
   useEffect(() => {
-    console.log("App: initAuth called, current path:", location.pathname);
+    console.log("App: initAuth called");
     initAuth();
   }, [initAuth]);
 
-  console.log(
-    "App render, isAuthenticated:",
-    isAuthenticated,
-    "path:",
-    location.pathname
-  );
+  if (isSessionChecking) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   return (
     <AppInit>
       <AppRoutes />
+      <ToastContainer position="top-right" autoClose={3000} />
     </AppInit>
   );
 }
